6e2a5e5ef4740a33e8cd99d71f3969831416a7f9
Tamara Zoner <tamarazoner@hotmail.com>
2017-04-18 15:05:16 -0300

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Jogada3 = new System.Windows.Forms.Label();
+            this.Jogada33 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Jogada2 = new System.Windows.Forms.Label();
+            this.Jogada1 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.Jogada11 = new System.Windows.Forms.Label();
+            this.Jogada22 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Jogar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Jogador 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(516, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Jogador 2:";
+            // 
+            // Jogada3
+            // 
+            this.Jogada3.AutoSize = true;
+            this.Jogada3.Location = new System.Drawing.Point(289, 104);
+            this.Jogada3.Name = "Jogada3";
+            this.Jogada3.Size = new System.Drawing.Size(13, 13);
+            this.Jogada3.TabIndex = 3;
+            this.Jogada3.Text = "0";
+            this.Jogada3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Jogada33
+            // 
+            this.Jogada33.AutoSize = true;
+            this.Jogada33.Location = new System.Drawing.Point(693, 104);
+            this.Jogada33.Name = "Jogada33";
+            this.Jogada33.Size = new System.Drawing.Size(13, 13);
+            this.Jogada33.TabIndex = 4;
+            this.Jogada33.Text = "0";
+            this.Jogada33.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(302, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Vencedor: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(367, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "------";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Jogada 1:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(117, 104);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Jogada 2:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(229, 104);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Jogada 3:";
+            // 
+            // Jogada2
+            // 
+            this.Jogada2.AutoSize = true;
+            this.Jogada2.Location = new System.Drawing.Point(177, 104);
+            this.Jogada2.Name = "Jogada2";
+            this.Jogada2.Size = new System.Drawing.Size(13, 13);
+            this.Jogada2.TabIndex = 10;
+            this.Jogada2.Text = "0";
+            // 
+            // Jogada1
+            // 
+            this.Jogada1.AutoSize = true;
+            this.Jogada1.Location = new System.Drawing.Point(68, 104);
+            this.Jogada1.Name = "Jogada1";
+            this.Jogada1.Size = new System.Drawing.Size(13, 13);
+            this.Jogada1.TabIndex = 11;
+            this.Jogada1.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(404, 104);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 13);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Jogada 1:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(516, 104);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(54, 13);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "Jogada 2:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(633, 104);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(54, 13);
+            this.label14.TabIndex = 14;
+            this.label14.Text = "Jogada 3:";
+            // 
+            // Jogada11
+            // 
+            this.Jogada11.AutoSize = true;
+            this.Jogada11.Location = new System.Drawing.Point(464, 104);
+            this.Jogada11.Name = "Jogada11";
+            this.Jogada11.Size = new System.Drawing.Size(13, 13);
+            this.Jogada11.TabIndex = 15;
+            this.Jogada11.Text = "0";
+            // 
+            // Jogada22
+            // 
+            this.Jogada22.AutoSize = true;
+            this.Jogada22.Location = new System.Drawing.Point(576, 104);
+            this.Jogada22.Name = "Jogada22";
+            this.Jogada22.Size = new System.Drawing.Size(13, 13);
+            this.Jogada22.TabIndex = 16;
+            this.Jogada22.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(352, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(9, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "|";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 284);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Jogada22);
+            this.Controls.Add(this.Jogada11);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.Jogada1);
+            this.Controls.Add(this.Jogada2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Jogada33);
+            this.Controls.Add(this.Jogada3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Jogo dado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Jogada3;
+        private System.Windows.Forms.Label Jogada33;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label Jogada2;
+        private System.Windows.Forms.Label Jogada1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label Jogada11;
+        private System.Windows.Forms.Label Jogada22;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd1 = new Random();
+          //  Random rnd2 = new Random();
+            
+            int PontosJogador1 = 0;
+            int PontosJogador2 = 0;
+            int valor1 = 0;
+            int valor2 = 0;
+            int i = 0;
+
+            for (i=0; i<3; i++)
+            {
+                if (i == 0)
+                {
+                    valor1 = rnd1.Next(1, 7);
+                    Jogada1.Text = " " + valor1;
+
+                    valor2 = rnd1.Next(1, 7);
+                    Jogada11.Text = " " + valor2;
+
+                    if (valor1 != valor2)
+                    {
+                        if (valor1 > valor2)
+                        {
+                            PontosJogador1 = PontosJogador1 + 1;
+                        }
+                        else
+                        {
+                            PontosJogador2 = PontosJogador2 + 1;
+                        }
+                    }
+                }
+                else if (i == 1)
+                {
+                    valor1 = rnd1.Next(1, 7);
+                    Jogada2.Text = " " + valor1;
+
+                    valor2 = rnd1.Next(1, 7);
+                    Jogada22.Text = " " + valor2;
+
+                    if (valor1 != valor2)
+                    {
+                        if (valor1 > valor2)
+                        {
+                            PontosJogador1 = PontosJogador1 + 1;
+                        }
+                        else
+                        {
+                            PontosJogador2 = PontosJogador2 + 1;
+                        }
+                    }
+
+                } else if (i == 2)
+                {
+                    valor1 = rnd1.Next(1, 7);
+                    Jogada3.Text = " " + valor1;
+
+                    valor2 = rnd1.Next(1, 7);
+                    Jogada33.Text = " " + valor2;
+
+                    if (valor1 != valor2)
+                    {
+                        if (valor1 > valor2)
+                        {
+                            PontosJogador1 = PontosJogador1 + 1;
+                        }
+                        else
+                        {
+                            PontosJogador2 = PontosJogador2 + 1;
+                        }
+                    }
+                }
+                
+                
+             }
+
+               
+             if (PontosJogador1 != PontosJogador2)
+             {
+                if (PontosJogador1 > PontosJogador2)
+                {
+                    label6.Text = "Jogador 1 Venceu  ";
+                }
+                else
+                {
+                    label6.Text = "Jogador 2 Venceu  ";
+                }
+             }
+             else
+             {
+                label6.Text = "Deu empate";
+             }
+                
+        }
+
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
